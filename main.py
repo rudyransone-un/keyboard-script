ed64824b7579810a687e34b1109439e5f9274048
rudyransone-un <rudyransone@gmail.com>
2023-01-31 19:54:38 +0300

    initial project

---
+++ main.py
@@ -0,0 +1,8 @@
+import keyboard
+
+
+keyboard.add_hotkey('shift+up', print, args=('hello,', ))
+# keyboard.add_hotkey('shift+up', lambda: keyboard.write('/'))
+# keyboard.add_hotkey('ctrl+up', lambda: keyboard.write('?'))
+# keyboard.add_hotkey('shift+esc', lambda: keyboard.write('~'))
+# keyboard.add_hotkey('caps+esc', lambda: keyboard.write('`'))
